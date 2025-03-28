41ee971ef1be4fb3f0ab1e82e09d4cdcd8bf2df4
Sala K 403 <salak403@salasfr.itm.edu.co>
2025-03-27 21:35:22 -0500

    parcial 2

---
+++ ObjPc.java
@@ -0,0 +1,71 @@
+public class ObjPc{
+    private int serial;
+    private String marca;
+    private int memoriaRam;
+    private int discoDuro;
+    private int precio;
+    private String NomUsuario;
+    private boolean disponible;
+
+    public ObjPc() {
+    }
+
+    public int getSerial() {
+        return serial;
+    }
+
+    public void setSerial(int serial) {
+        this.serial = serial;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getMemoriaRam() {
+        return memoriaRam;
+    }
+
+    public void setMemoriaRam(int memoriaRam) {
+        this.memoriaRam = memoriaRam;
+    }
+
+    public int getDiscoDuro() {
+        return discoDuro;
+    }
+
+    public void setDiscoDuro(int discoDuro) {
+        this.discoDuro = discoDuro;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public String getNomUsuario() {
+        return NomUsuario;
+    }
+
+    public void setNomUsuario(String nomUsuario) {
+        NomUsuario = nomUsuario;
+    }
+
+    public boolean getDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+    
+    
+
+}
---
+++ ObjTablet.java
@@ -0,0 +1,63 @@
+public class ObjTablet {
+    private int serial;
+    private int tamaño;
+    private String marca;
+    private int precio;
+    private String NomUsuario;
+    private boolean disponible;
+
+    public ObjTablet() {
+    }
+
+    public int getSerial() {
+        return serial;
+    }
+
+    public void setSerial(int serial) {
+        this.serial = serial;
+    }
+
+    public int getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(int tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public String getNomUsuario() {
+        return NomUsuario;
+    }
+
+    public void setNomUsuario(String nomUsuario) {
+        NomUsuario = nomUsuario;
+    }
+
+    public boolean getDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+    
+
+    
+}
+
